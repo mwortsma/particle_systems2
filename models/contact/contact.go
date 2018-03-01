ac1e17428e208c0e7f73bebfaff8a89e82c7f773
mitchell <mitchellwortsman@gmail.com>
2018-03-01 13:34:47 -0500

    oops -- last so wrong

--- models/contact/contact.go
+++ models/contact/contact.go
@@ -7,7 +7,7 @@ import 	(
 )
 
 // General.
-func getLawQ(p,q float64, d,k int) probutil.LawTransition {
+func getLawQ(p,q float64) probutil.LawTransition {
   // transition from s to s_new
   return func(s_new, s int, f probutil.Law) float64 {
   		if s == 1 {
@@ -27,52 +27,51 @@ func getLawQ(p,q float64, d,k int) probutil.LawTransition {
   	}
 }
 
-func getNeighborQ(p,q float64, d,k int) probutil.NeighborTransition {
-  return probutil.GetNeighborTransition(getLawQ(p,q,d,k),k)
+func getNeighborQ(p,q float64) probutil.NeighborTransition {
+  return probutil.GetNeighborTransition(getLawQ(p,q),2)
 }
 
 // Dense.
-func DenseTimeDistr(T int,
-  Q probutil.NeighborTransition,
+func DenseTimeDistr(
+  T int,
+  p,q float64,
   nu probutil.InitDistr,
-  k int,
   steps int,
   n int) probutil.TimeDistr {
 
-  return fullgraph.DenseTimeDistr(T,Q,nu,k,steps,n)
+  return fullgraph.DenseTimeDistr(T,getNeighborQ(p,q),nu,2,steps,n)
 
 }
 
 func DensePathDistr(
   T int,
-  Q probutil.NeighborTransition,
+  p,q float64,
   nu probutil.InitDistr,
-  k int,
   steps int,
   n int) probutil.PathDistr {
 
-  return fullgraph.DensePathDistr(T,Q,nu,k,steps,n)
+  return fullgraph.DensePathDistr(T,getNeighborQ(p,q),nu,2,steps,n)
 }
 
 // Tree
 func TreeTimeDistr(
   T int,
+  p,q float64,
   d int,
-  Q probutil.NeighborTransition,
   nu probutil.InitDistr,
-  k int,
 	steps int) probutil.TimeDistr {
 
-  return fulltree.TimeDistr(T,d,Q,nu,k,steps)
+  return fulltree.TimeDistr(T,d,getNeighborQ(p,q),nu,2,steps)
 }
 
 func TreePathDistr(
   T int,
+  p,q float64,
   d int,
-  Q probutil.NeighborTransition,
   nu probutil.InitDistr,
-  k int,
 	steps int) probutil.PathDistr {
 
-  return fulltree.PathDistr(T,d,Q,nu,k,steps)
+  return fulltree.PathDistr(T,d,getNeighborQ(p,q),nu,2,steps)
 }
+
+// Local
